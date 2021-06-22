7cf9bd29d45e9f193c91b4d600457addea1f158a
OgelGames <48543043+OgelGames@users.noreply.github.com>
2021-06-22 16:13:50 +1000

    big rewrite commit

--- init.lua
+++ init.lua
@@ -7,44 +7,43 @@ A mod which provides player-placed teleport pads
 
 Copyright (C) 2015 Ben Deutsch <ben@bendeutsch.de>
 
-License
--------
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-USA
-
 ]]
 
 telemosaic = {
-    -- configuration
-    config = {
-        -- keep emerge_delay lower than teleport_delay!
-        emerge_delay = 0.5, -- seconds
-        teleport_delay = tonumber(minetest.settings:get("telemosaic_teleport_delay")) or 2.0, -- seconds
-        beacon_range = tonumber(minetest.settings:get("telemosaic_beacon_range")) or 20.0, -- max teleport distance
-        extender_ranges = {
-            -- note: not adding beacons here, since they don't extend
-            -- also: base name of colored versions
-            ['telemosaic:extender_one'] = tonumber(minetest.settings:get("telemosaic_extender_one_range")) or 5.0,
-            ['telemosaic:extender_two'] = tonumber(minetest.settings:get("telemosaic_extender_two_range")) or 20.0,
-            ['telemosaic:extender_three'] = tonumber(minetest.settings:get("telemosaic_extender_three_range")) or 80.0,
-        },
-    },
+	teleport_delay = tonumber(minetest.settings:get("telemosaic_teleport_delay")) or 1.0,
+	beacon_range = tonumber(minetest.settings:get("telemosaic_beacon_range")) or 20.0,
+	default_channel = minetest.settings:get("telemosaic_default_channel") or "telemosaic",
+	extender_ranges = {
+		tonumber(minetest.settings:get("telemosaic_extender_one_range")) or 5.0,
+		tonumber(minetest.settings:get("telemosaic_extender_two_range")) or 20.0,
+		tonumber(minetest.settings:get("telemosaic_extender_three_range")) or 80.0,
+	},
+	extender_colors = {
+		white = "#ffffffa0",
+		dark_grey = "#00000090",
+		black = "#000000e0",
+		violet = "#400080b0",
+		blue = "#0000ffb0",
+		cyan = "#00ffffb0",
+		dark_green = "#007000b0",
+		green = "#00ff00b0",
+		yellow = "#ffff00b0",
+		brown = "#402000c0",
+		orange = "#ff8000b0",
+		red = "#ff0000b0",
+		magenta = "#ff00ffb0",
+		pink = "#ff8080b0",
+	},
 }
-local modpath = minetest.get_modpath(minetest.get_current_modname())
 
-dofile(modpath.."/teleport.lua")
-dofile(modpath.."/crafts.lua")
-dofile(modpath.."/abm.lua")
+local MP = minetest.get_modpath("telemosaic")
+
+if minetest.get_modpath("digilines") then
+	dofile(MP.."/digilines.lua")
+end
+
+dofile(MP.."/functions.lua")
+dofile(MP.."/beacon.lua")
+dofile(MP.."/extender.lua")
+dofile(MP.."/key.lua")
+dofile(MP.."/abm.lua")
